d09830e716f5afb1144904f8308a4de0883fa37c
Artem Bykov <artemio91@gmail.com>
2014-08-15 15:27:16 +0300

    Update examples

--- examples/index.php
+++ examples/index.php
@@ -4,12 +4,14 @@ require '../vendor/autoload.php';
 use \Hypem\Playlist as Playlist;
 use \Hypem\Track as Track;
 
-$latest   = Playlist::latest('noremix')->get();
+// $latest   = Playlist::latest('noremix');
 // $popular  = Playlist::popular()->get(3);
 // $artist   = Playlist::artist('Placebo')->get();
 // $blog     = Playlist::blog(16746)->get(); //"Cruel Rythm" blog_id
 // $tags     = Playlist::tags('electro house')->get();
 // $search   = Playlist::search('Woodkid Iron')->get();
+
+$track = new Track('2614n');
 ?>
 
-<audio src="<?php echo (new Track('2614n'))->get(); ?>" controls="controls"></audio>
+<audio src="<?php echo $track->get(); ?>" controls="controls"></audio>

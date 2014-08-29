f0f48612791c7f616aff038976a3255bc5ed3bc3
Artem Bykov <artemio91@gmail.com>
2014-08-29 13:02:46 +0300

    Fix spaces in Playlist class

--- src/Hypem/Playlist.php
+++ src/Hypem/Playlist.php
@@ -5,37 +5,37 @@ class Playlist
 {
     const PATH_TEMPLATE = '/playlist/{{type}}/{{filter}}/json/{{page}}';
 
-    const TYPE_LATEST           = 'latest';
-    const TYPE_POPULAR          = 'popular';
-    const TYPE_ARTIST           = 'artist';
-    const TYPE_BLOG             = 'blog';
-    const TYPE_TAGS             = 'tags';
-    const TYPE_SEARCH           = 'search';
-    const TYPE_TRACK            = 'track';
-    const TYPE_FEED             = 'feed';
-    const TYPE_LOVED            = 'loved';
-    const TYPE_HISTORY          = 'history';
-    const TYPE_OBSESSED         = 'obsessed';
-    const TYPE_PEOPLE           = 'people';
-    const TYPE_PEOPLE_OBSESSED  = 'people_obsessed';
+    const TYPE_LATEST          = 'latest';
+    const TYPE_POPULAR         = 'popular';
+    const TYPE_ARTIST          = 'artist';
+    const TYPE_BLOG            = 'blog';
+    const TYPE_TAGS            = 'tags';
+    const TYPE_SEARCH          = 'search';
+    const TYPE_TRACK           = 'track';
+    const TYPE_FEED            = 'feed';
+    const TYPE_LOVED           = 'loved';
+    const TYPE_HISTORY         = 'history';
+    const TYPE_OBSESSED        = 'obsessed';
+    const TYPE_PEOPLE          = 'people';
+    const TYPE_PEOPLE_OBSESSED = 'people_obsessed';
 
-    const FILTER_ALL        = 'all';
-    const FILTER_FRESH      = 'fresh';
-    const FILTER_REMIX      = 'remix';
-    const FILTER_NOREMIX    = 'noremix';
-    const FILTER_NOW        = 'now';
-    const FILTER_LASTWEEK   = 'lastweek';
-    const FILTER_ARTISTS    = 'artists';
-    const FILTER_TWITTER    = 'twitter';
+    const FILTER_ALL      = 'all';
+    const FILTER_FRESH    = 'fresh';
+    const FILTER_REMIX    = 'remix';
+    const FILTER_NOREMIX  = 'noremix';
+    const FILTER_NOW      = 'now';
+    const FILTER_LASTWEEK = 'lastweek';
+    const FILTER_ARTISTS  = 'artists';
+    const FILTER_TWITTER  = 'twitter';
 
-    private static $latest_filters = [
+    public static $latest_filters = [
         self::FILTER_ALL,
         self::FILTER_FRESH,
         self::FILTER_REMIX,
         self::FILTER_NOREMIX
     ];
 
-    private static $popular_filters = [
+    public static $popular_filters = [
         self::FILTER_NOW,
         self::FILTER_LASTWEEK,
         self::FILTER_REMIX,
@@ -127,9 +127,23 @@ class Playlist
         return new self(self::TYPE_PEOPLE_OBSESSED, $name);
     }
 
-    public function getData($page)
+    public function setPage($page)
     {
         $this->page = $page;
+    }
+
+    public function buildPath()
+    {
+        return strtr(self::PATH_TEMPLATE, [
+            '{{type}}'   => $this->type,
+            '{{filter}}' => $this->filter,
+            '{{page}}'   => $this->page
+        ]);
+    }
+
+    public function getData($page)
+    {
+        $this->setPage($page);
         $data = (new Request)->getJson($this->buildPath());
         unset($data['version']);
         return (array)$data;
@@ -140,14 +154,5 @@ class Playlist
         return array_map(function($datum) {
             return new Track($datum);
         }, $this->getData($page));
-    }
-
-    public function buildPath()
-    {
-        return strtr(self::PATH_TEMPLATE, [
-            '{{type}}'      => $this->type,
-            '{{filter}}'    => $this->filter,
-            '{{page}}'      => $this->page
-        ]);
     }
 }
